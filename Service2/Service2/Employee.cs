12f90970c395002c29ca54ed2a7b874e191cdd5c
kamranss <kamranss@code.edu.az>
2022-12-08 22:28:42 +0400

    Refactor: added new Service

---
+++ Service2/Service2/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service2.Service2
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
